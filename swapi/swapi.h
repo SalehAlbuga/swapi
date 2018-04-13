01a02d455a703a038fc4dd79c5073071b021d924
Saleh Albuga <saleh.albuga@arabiaweather.com>
2018-04-13 21:13:51 +0300

    Initial Commit

---
+++ swapi/swapi.h
@@ -0,0 +1,19 @@
+//
+//  swapi.h
+//  swapi
+//
+//  Created by Saleh on 4/13/18.
+//  Copyright © 2018 Saleh Albuga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swapi.
+FOUNDATION_EXPORT double swapiVersionNumber;
+
+//! Project version string for swapi.
+FOUNDATION_EXPORT const unsigned char swapiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swapi/PublicHeader.h>
+
+
